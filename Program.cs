1cbba14e0e9cefce7c092277066c82b5cb563977
Łukasz Łaszczewski <spartan282@wp.pl>
2023-06-27 16:37:53 +0200

    Zadanie Dzień 4

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Cześć Łukasz");
+﻿var name1 = "Łukasz";
+var name2 = "Ewa";
+
+var sex1 = "Mężczyzna";
+var sex2 = "Kobieta";
+
+var age1 = 17;
+var age2 = 33;
+
+if (name2 == "Ewa" && age2 == 33)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+
+else if (name2 != "Ewa" && sex2 == "Kobieta" && age2 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+
+else if (sex1 == "Mężczyzna" && age1 < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
