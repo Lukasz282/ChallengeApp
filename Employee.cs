a58f28613d4e07ce38a079e1c69339383673958f
Łukasz Łaszczewski <spartan282@wp.pl>
2024-11-05 12:05:11 +0100

    Klasa employee, pętle

--- Employee.cs
+++ Employee.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ChallengeApp
 {
 
     public class Employee
     {
-        
-        private List<int> score = new List<int>();
+        private List<float> score = new List<float>();
 
         public Employee(string name, string last_name, string age)
         {
@@ -21,20 +24,104 @@ namespace ChallengeApp
         public string Name { get; private set; }
         public string Last_Name { get; private set; }
         public string Age { get; private set; }
-        public int Result
+     
+
+
+        public void AddScore(float score)
+        {
+            //Walidacja dodawanie warunków działania programu
+            //Można dodawać oceny od 0 do 100
+
+            if (score >= 0 && score <= 100)
+            {
+                this.score.Add(score);
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowa wartość oceny");
+            }
+
+        }
+        // Dodanie metody o tej samej nazwie ale innym parametrze
+        // Teraz wpisując string program przerabia go na float
+
+        public void AddScore(string score)
+        {
+            if(float.TryParse(score, out float result))
+            {
+                this.score.Add(result);
+            }
+            else
+            {
+                Console.WriteLine("To nie jest ocena");
+            }
+            var value = float.Parse(score);
+            this.AddScore(value);
+        }
+        public void AddScore(int score)
+        {
+            if (score >=0 && score <= 100)
+            {
+                this.score.Add(score);
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowa wartość oceny");
+            }
+        }
+        public void AddScore (long score)
+        {
+            if (score >= 0 && score <= 100)
+            {
+                this.score.Add(score);
+            }
+        }
+        public void AddScore (double score)
+        {
+          float floatvalue = (float)score;
+          this.score.Add(floatvalue);
+        }
+
+        public float Result
         {
             get
             {
                 return this.score.Sum();
+                    
             }
         }
-
-
-        public void AddScore(int score)
+  
+       
+        public Statistics GetStatistics()
         {
-            this.score.Add(score);
-        }
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
 
+           
+
+            return statistics;
+
+
+
+            foreach (var score in this.score)
+            {
+                statistics.Max = Math.Max(statistics.Max, score);
+                statistics.Min =Math.Min(statistics.Min, score);
+                statistics.Average = +score;
+            }
+
+            statistics.Average /= this.score.Count;
+            return statistics;
+
+            
+
+
+
+        }
+        
+       
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,47 +5,15 @@
 
 using ChallengeApp;
 
-Employee employee1 = new Employee("Adam", "Kowalski", "30");
-Employee employee2 = new Employee("Krzysztof", "Kwiatkowski", "35");
-Employee employee3 = new Employee("Joanna", "Nowak", "27");
 
-employee1.AddScore(4);
-employee1.AddScore(3);
-employee1.AddScore(7);
-employee1.AddScore(3);
-employee1.AddScore(7);
+var employee = new Employee("Jan", "Kowalski", "30");
+employee.AddScore("2");
+employee.AddScore(2);
+employee.AddScore(6);
 
-employee2.AddScore(7);
-employee2.AddScore(10);
-employee2.AddScore(3);
-employee2.AddScore(10);
-employee2.AddScore(3);
+var statistics = employee.GetStatistics();
 
-employee3.AddScore(10);
-employee3.AddScore(5);
-employee3.AddScore(10);
-employee3.AddScore(5);
-employee3.AddScore(10);
-
-List<Employee> employees = new List<Employee>()
-{
-    employee1, employee2, employee3
-};
-
-int maxResult = -1;
-Employee employeeWithMaxResult = null;
-
-foreach (var employee in employees )
-{
-    if (employee.Result > maxResult)
-    {
-        maxResult = employee.Result;
-        employeeWithMaxResult = employee;
-    }
-}
-
-Console.WriteLine(employeeWithMaxResult.Name);
-Console.WriteLine(employeeWithMaxResult.Last_Name);
-Console.WriteLine(employeeWithMaxResult.Age);
-
+Console.WriteLine($"Max:{statistics.Max}");
+Console.WriteLine($"Min:{statistics.Min}");
+Console.WriteLine($"Average:{statistics.Average:N2}");
 
